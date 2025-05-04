c0513d6e3905cfa56d0d92d7e60b1329f4814883
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-05-03 19:57:09 -0500

    Agregar hook para obtener data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Forecast from "./components/Forecast";
 import Highlights from "./components/Highlights";
 import Main from "./components/Main";
+import useData from "./hooks/useData";
 
 function App() {
+  const API_KEY = import.meta.env.VITE_WEATHER_KEY;
+  const [units, setUnits] = useState("metric");
+  const { loading, response } = useData(
+    `https://api.openweathermap.org/data/2.5/weather?q=london&units=${units}&appid=${API_KEY}`
+  );
+  console.log(!loading && response);
+
   return (
     <div className="bg-[#1E213A] w-full min-h-screen flex flex-col items-center md:flex-row md:items-start font-weather">
       <Main />
